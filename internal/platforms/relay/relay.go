52b683a6e0e5e3fa54c6eadbc771e3162b266e43
hophacker <jiefeng.hopkins@gmail.com>
2026-03-07 04:29:24 +0800

    chore: version 1.9.5

--- internal/platforms/relay/relay.go
+++ internal/platforms/relay/relay.go
@@ -25,7 +25,7 @@ import (
 const (
 	DefaultServerURL  = "wss://bot.lingti.com/ws"
 	DefaultWebhookURL = "https://bot.lingti.com/webhook"
-	ClientVersion     = "1.9.4"
+	ClientVersion     = "1.9.5"
 
 	writeTimeout      = 10 * time.Second
 	readTimeout       = 60 * time.Second
